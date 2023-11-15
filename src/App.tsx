c98e66e08063faa47fc3fc15ff733f22a7b160dd
Nikita Kukharchuk <tyrynmyryn@gmail.com>
2023-11-15 09:59:40 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react';
+
+const App = () => {
+  // const [state, setState] = useState();
+
+  return (
+    <div>
+      <div className='max-w-xl bg-black'>App</div>
+    </div>
+  );
+};
+
+export default App;
